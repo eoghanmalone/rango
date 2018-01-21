c2f3e2a5cc9a1cd4bacd76640bb5370dadfd24bf
Eoghan Malone <2227056m@student.gla.ac.uk>
2018-01-21 17:11:12 +0000

    Most viewed pages

--- populate_rango.py
+++ populate_rango.py
@@ -8,36 +8,37 @@ from rango.models import Category, Page
 
 def populate():
 
+
     python_pages = [
 
-		{"title": "Official Python Tutorial",
-                 "url":"http://docs.python.org/2/tutorial/"},
-		{"title":"How to Think like a Computer Scientist",
-                 "url":"http://www.greenteapress.com/thinkpython/"},
-                {"title":"Learn Python in 10 Minutes",
-                 "url":"http://www.korokithakis.net/tutorials/python/"} ]
-
+    {"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+    {"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},  ]
+    
     django_pages = [
 
 		{"title":"Official Django Tutorial",
-	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
 		{"title":"Django Rocks",
-	        "url":"http://www.djangorocks.com/"},
+	        "url":"http://www.djangorocks.com/" , "views": 16},
 		{ "title":"How to Tango with Django",
-	        "url":"http://www.tangowithdjango.com/"} ]
+	        "url":"http://www.tangowithdjango.com/", "views": 8 } ]
 
     other_pages = [
 
-		{ "title":"Bottle",
-                  "url":"http://bottlepy.org/docs/dev/"},
-		{ "title":"Flask",
-                  "url":"http://flask.pocoo.org"} ]
-
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16} ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+
 	    "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16},
-            }    
+            "Pascal": {"pages": [], "views": 32, "likes": 16},
+            "Perl": {"pages": [], "views": 32, "likes": 16},
+            "Php": {"pages": [], "views": 32, "likes": 16},
+            "Prolog": {"pages": [], "views": 32, "likes": 16},
+            "Programming": {"pages": [], "views": 32, "likes": 16},
+            }   
 
     # if you want to add more catergories or pages, add them to the dictionaries above
     # The code below goes through the cats dictionary, then adds each category,
@@ -56,7 +57,9 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
+
 def add_page(cat, title, url, views=0):
+
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
