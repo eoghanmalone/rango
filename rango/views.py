625cb3287614f5c884ec4786e90acd67d6096f88
Eoghan Malone <2227056m@student.gla.ac.uk>
2018-01-21 16:17:27 +0000

    Python page test

--- rango/views.py
+++ rango/views.py
@@ -37,7 +37,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = pages
         # We also add the category object from the database to the context dictionary
         # We'll use this in the template to verify that the category existss
-        contect_dict['category'] = category
+        context_dict['category'] = category
 
     except Category.DoesNotExist:
         #We get here if we didn't find the specified category
