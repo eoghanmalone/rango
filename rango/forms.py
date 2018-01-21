81c1a3e75062a43ee5804bcefeafba323da04d65
Eoghan Malone <2227056m@student.gla.ac.uk>
2018-01-21 18:04:05 +0000

    Add category code added

--- rango/forms.py
+++ rango/forms.py
@@ -9,9 +9,9 @@ class CategoryForm(forms.ModelForm):
 
     # An inline class to provide additional information on the form.
     class Meta:
-    # Provide an association between the ModelForm and a model
-    model = Category
-    fields = ('name',)
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
@@ -21,14 +21,14 @@ class PageForm(forms.ModelForm):
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
-    # Provide an association between the ModelForm and a model
-    model = Page
+        # Provide an association between the ModelForm and a model
+        model = Page
 
-    # What fields do we want to include in our form?
-    # This way we don't need every field in the model present.
-    # Some fields may allow NULL values, so we may not want to include them.
-    # Here, we are hiding the foreign key.
-    # we can either exclude the category field from the form,
-    exclude = ('category',)
-    # or specify the fields to include (i.e. not include the category field)
-    #fields = ('title', 'url', 'views')
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values, so we may not want to include them.
+        # Here, we are hiding the foreign key.
+        # we can either exclude the category field from the form,
+        exclude = ('category',)
+        # or specify the fields to include (i.e. not include the category field)
+        #fields = ('title', 'url', 'views')
